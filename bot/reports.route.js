f7cc655658fa3444b19aab7e088a35b2934ae05c
Original-pokemon <eremkin-02@mail.ru>
2022-08-01 14:33:37 +0300

    fix: regexp callbackQuery

--- bot/reports.route.js
+++ bot/reports.route.js
@@ -6,8 +6,8 @@ module.exports = function (
   downloadPhoto
 ) {
   botInstance.callbackQuery('reports', main)
-  botInstance.callbackQuery(/date_[a-zA-Z_]+/g, sendDate)
-  botInstance.callbackQuery(/reports_\w+\\\d+-\d-\d+/g, sendReports)
+  botInstance.callbackQuery(/date_[\wа-яА-Я\d]+/g, sendDate)
+  botInstance.callbackQuery(/reports_[\wа-яА-Я\d]+\\\d+-\d-\d+/g, sendReports)
 
   botInstance.on(':photo', downloadPhoto)
 }
